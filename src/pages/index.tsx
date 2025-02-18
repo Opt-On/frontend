bd39092a2de4f27c6542835b104cc3b0c78f6982
owen-sellner <owen.sellner@gmail.com>
2025-02-18 16:44:57 -0500

    add placeholder text to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { Button } from "@primer/react";
+import { Text } from "@primer/react";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
@@ -7,10 +6,13 @@ export default function Home() {
         <main>
             <section>
                 <NavBar />
-                <div>
-                    <h1>Home</h1>
-                    <Link style={{ textDecoration: "none", color: "inherit" }} href="/transcript">Transcript Page</Link>
-                    <Button variant="primary">Button</Button>
+                <div style={{ padding: "6rem", width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <Text as="h1" style={{ fontWeight: 600, fontSize: 32, lineHeight: "150%" }}>
+                        This is our super cool headline.
+                    </Text>
+                    <Text as="h2" style={{ color: "#57606A", fontWeight: 400, fontSize: 20 }}>
+                        Making your option planning easier or whatever
+                    </Text>
                 </div>
             </section>
         </main>
