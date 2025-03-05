c1c75ce026c9d13035e4967d61b7c9906b425a13
ericzhang11101 <ericrenzhang321@gmail.com>
2025-03-05 15:46:39 -0500

    stupid

--- src/components/modals/ProfileModal.tsx
+++ src/components/modals/ProfileModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useAuth } from "@/context";
 import { Box, Button, Dialog, Text, TextInput } from "@primer/react";
 import { useRouter } from "next/router";
