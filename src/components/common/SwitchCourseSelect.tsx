626b67e0a74e16cbcbd887948f51ddd5f624a819
ericzhang11101 <ericrenzhang321@gmail.com>
2025-03-22 17:48:50 -0400

    yeet

---
+++ src/components/common/SwitchCourseSelect.tsx
@@ -0,0 +1,37 @@
+import { SyncIcon } from "@primer/octicons-react";
+import { ActionList, ActionMenu, Box } from "@primer/react";
+import styles from "./ActionSelect.module.scss";
+import { RecommendedCourse } from "./OptionProgressDetailed";
+
+export default function SwitchCourseSelect({
+  handleSetSelected,
+  courseList,
+}: {
+  handleSetSelected: (course: string) => void;
+  courseList: RecommendedCourse[];
+}) {
+  return (
+    <ActionMenu>
+      <ActionMenu.Button leadingVisual={SyncIcon}>
+        <Box className={styles.button}>Switch</Box>
+      </ActionMenu.Button>
+      <ActionMenu.Overlay side="inside-center">
+        <Box className={styles.overlay}>
+          <ActionList>
+            {courseList.map((course, index) => (
+              <ActionList.LinkItem
+                key={`courseList-${index}`}
+                className={styles.linkItem}
+                onClick={() => handleSetSelected(course.name)}
+              >
+                <span>
+                  {course.name} - {course.description}
+                </span>
+              </ActionList.LinkItem>
+            ))}
+          </ActionList>
+        </Box>
+      </ActionMenu.Overlay>
+    </ActionMenu>
+  );
+}
