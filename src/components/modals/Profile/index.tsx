d6718f16929363ec536b7dfa00c5d213adb2b499
owen-sellner <owen.sellner@gmail.com>
2025-03-21 22:29:40 -0400

    add avatars

--- src/components/modals/Profile/index.tsx
+++ src/components/modals/Profile/index.tsx
@@ -1,19 +1,64 @@
 import { Dialog, Box, IconButton, Text, Button, TextInput } from "@primer/react";
-import { XIcon } from "@primer/octicons-react";
+import { CheckIcon, XIcon } from "@primer/octicons-react";
 import styles from "./Profile.module.scss";
 import FileUpload from "@/components/FileUpload";
 import { useAuth } from "@/context";
+import Image from "next/image";
+import { useState } from "react";
 
 type ProfileProps = {
   handleClose: (gesture: "close-button" | "escape") => void;
 };
 
 export const Profile: React.FC<ProfileProps> = ({ handleClose }) => {
-  const { user, userInfo } = useAuth();
+  const { user, userInfo, logout } = useAuth();
+
+  const [indexA, setIndexA] = useState(-1);
+  const [indexB, setIndexB] = useState(-1);
+
+  const handleSetA = (index: number) => {
+    setIndexA(index);
+    if (indexB === -1) {
+      setIndexB(0);
+    }
+  };
+
+  const handleSetB = (index: number) => {
+    setIndexB(index);
+    if (indexA === -1) {
+      setIndexA(0);
+    }
+  };
+
+  const emojis = ["🐱", "🐶", "🐰", "🐻", "🐻‍❄️", "🐐", "🐮"];
+  const bgColors = ["#fbefff", "#ffeff7", "#ddf4ff", "#dafbe1", "#fff8c5", "#fff1e5", "#ffebe9"];
+  const inactiveColors = [
+    "rgba(130, 80, 223, 0.5)",
+    "rgba(191, 57, 137, 0.5)",
+    "rgba(9, 105, 218, 0.5)",
+    "rgba(26, 127, 55, 0.5)",
+    "rgba(154, 103, 0, 0.5)",
+    "rgba(188, 76, 0, 0.5)",
+    "rgba(207, 34, 46, 0.5)",
+  ];
+  const activeColors = [
+    "#8250df",
+    "#bf3989",
+    "#0969da",
+    "#1a7f37",
+    "#9a6700",
+    "#bc4c00",
+    "#cf222e",
+  ];
+
+  const handleLogout = () => {
+    handleClose("escape");
+    logout();
+  };
 
   const Header = () => (
     <Box className={styles.header}>
-      <Box style={{ display: "flex", justifyContent: "space-between" }}>
+      <Box display='flex' justifyContent='space-between'>
         <Text as='h1' className={styles.title}>
           Profile
         </Text>
@@ -22,7 +67,7 @@ export const Profile: React.FC<ProfileProps> = ({ handleClose }) => {
           size='large'
           icon={() => <XIcon size={20} />}
           variant='invisible'
-          aria-labelledby='close'
+          aria-label='close'
         />
       </Box>
     </Box>
@@ -31,76 +76,131 @@ export const Profile: React.FC<ProfileProps> = ({ handleClose }) => {
   const Body = () => (
     <Box className={styles.body}>
       <Box display='flex' flexDirection='column'>
-        <Text fontSize='16px' weight='semibold'>
+        <Text fontSize={16} fontWeight='semibold'>
           Avatar
         </Text>
-        {user?.photoURL && (
-          <div>
-            <img
-              src={user.photoURL}
-              height={115}
+        <Box style={{ display: "flex", marginTop: 16 }}>
+          {indexA === -1 ? (
+            <Image
+              src={user?.photoURL || "https://avatars.githubusercontent.com/u/7143434?v=4"}
               width={115}
-              style={{ borderRadius: "50%", marginTop: "16px" }}
+              height={115}
               alt='avatar'
+              style={{ borderRadius: "50%" }}
             />
-          </div>
-        )}
-      </Box>
-      <Box display='flex' flexDirection='column'>
-        <Text fontSize='16px' weight='semibold'>
-          Basic Information
-        </Text>
-        <Text fontSize='14px' color='#656d76'>
-          To update basic information please re-upload your transcript.
-        </Text>
-        <Box
-          display='grid'
-          gridTemplateColumns='repeat(2, 1fr)'
-          marginTop='0.75rem'
-          sx={{ columnGap: "1rem", rowGap: "0.25rem", gap: "16px" }}
-        >
-          <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            <Text fontSize='14px' weight='semibold'>
-              First Name
-            </Text>
-            <TextInput
-              style={{ cursor: "auto" }}
-              value={userInfo?.firstName || ""}
-              disabled
-              block
-            />
-          </div>
-          <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            <Text fontSize='14px' weight='semibold'>
-              Last Name
-            </Text>
-            <TextInput style={{ cursor: "auto" }} value={userInfo?.lastName || ""} disabled block />
-          </div>
-          <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            <Text fontSize='14px' weight='semibold'>
-              Program
-            </Text>
-            <TextInput style={{ cursor: "auto" }} value={userInfo?.program || ""} disabled block />
-          </div>
-          <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            <Text fontSize='14px' weight='semibold'>
-              Email
-            </Text>
-            <TextInput style={{ cursor: "auto" }} value={user?.email || ""} disabled block />
-          </div>
+          ) : (
+            <Box
+              width={115}
+              height={115}
+              borderRadius='50%'
+              bg={bgColors[indexB]}
+              display='flex'
+              alignItems='center'
+              justifyContent='center'
+              fontSize='80px'
+            >
+              {emojis[indexA]}
+            </Box>
+          )}
+          <Box
+            display='grid'
+            gridTemplateColumns='repeat(7, 1fr)'
+            gridTemplateRows='repeat(2, 1fr)'
+            width='75%'
+            margin='0 auto'
+            alignItems='center'
+            justifyItems='center'
+          >
+            {Array.from({ length: 7 }).map((_, index) => (
+              <Box
+                key={index}
+                width={36}
+                height={36}
+                borderRadius='50%'
+                bg='#f6f8fa'
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                fontSize='20px'
+                paddingTop='2px'
+                style={{ cursor: "pointer" }}
+                border='solid 2px'
+                borderColor={indexA === index ? "rgba(110, 119, 129, 0.5)" : "transparent"}
+                onClick={() => handleSetA(index)}
+              >
+                {emojis[index]}
+              </Box>
+            ))}
+            {Array.from({ length: 7 }).map((_, index) => (
+              <Box
+                key={index}
+                width={36}
+                height={36}
+                borderRadius='50%'
+                bg={bgColors[index]}
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                fontSize='20px'
+                paddingTop='2px'
+                style={{ cursor: "pointer" }}
+                color={indexB === index ? activeColors[index] : inactiveColors[index]}
+                border='solid 2px'
+                borderColor={indexB === index ? inactiveColors[index] : "transparent"}
+                onClick={() => handleSetB(index)}
+              >
+                <CheckIcon size={20} />
+              </Box>
+            ))}
+          </Box>
         </Box>
       </Box>
       <Box display='flex' flexDirection='column'>
-        <Text size='large' weight='semibold'>
+        <Text fontSize={16} fontWeight='semibold'>
+          Basic Information
+        </Text>
+        <Text fontSize={14} color='#656d76'>
+          To update basic information please re-upload your transcript.
+        </Text>
+        <Box display='grid' gridTemplateColumns='repeat(2, 1fr)' mt={3} sx={{ gap: 2 }}>
+          {["firstName", "lastName", "program", "email"].map((field) => (
+            <Box key={field} className={styles.flexColumn}>
+              <Text fontSize={14} fontWeight='semibold'>
+                {field === "email"
+                  ? "Email"
+                  : field
+                      .split(/(?=[A-Z])/)
+                      .join(" ")
+                      .replace(/\b\w/g, (char) => char.toUpperCase())}{" "}
+              </Text>
+              <TextInput
+                value={
+                  field === "email"
+                    ? user?.[field] || ""
+                    : userInfo?.[field as keyof typeof userInfo] || ""
+                }
+                disabled
+                block
+                style={{ cursor: "auto" }}
+              />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+      <Box display='flex' flexDirection='column'>
+        <Text fontSize={16} fontWeight='semibold'>
           Upload Transcript
         </Text>
         {userInfo?.uploadDate && (
-          <Text fontSize='14px' color='#656d76'>
+          <Text fontSize={14} color='#656d76'>
             Last uploaded on {userInfo.uploadDate}
           </Text>
         )}
         <FileUpload />
       </Box>
+      <Button onClick={handleLogout} variant='danger' sx={{ width: 76 }}>
+        Log out
+      </Button>
     </Box>
   );
 
@@ -108,7 +208,7 @@ export const Profile: React.FC<ProfileProps> = ({ handleClose }) => {
     <Dialog
       width='xlarge'
       onClose={handleClose}
-      title='Login or Sign Up'
+      title='Profile'
       renderHeader={Header}
       renderBody={Body}
     />
