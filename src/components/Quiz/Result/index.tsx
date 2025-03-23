d3a79cc5a5d716f1b4764e7819240a3bc2cab3d2
hoeddie <e52ho@uwaterloo.ca>
2025-03-22 21:53:58 -0400

    added images

--- src/components/Quiz/Result/index.tsx
+++ src/components/Quiz/Result/index.tsx
@@ -7,19 +7,22 @@ interface ResultProps {
 }
 
 export default function Result({ program, handleClose }: ResultProps) {
+  const imageSrc = `/quiz_results/${program.toLowerCase().replace(/\s+/g, "_")}.png`;
+
   return (
     <div className={styles.container}>
       <div>
-        <Text as='p' className={styles.text}>
+        <Text as="p" className={styles.text}>
           Oh wow! You&#39;d be a great fit for the
         </Text>
-        <Text as='h1' className={styles.heading}>
+        <Text as="h1" className={styles.heading}>
           {program} Option
         </Text>
       </div>
-      <div className={styles.imagePlaceholder} />
-      <Button className={styles.button} onClick={handleClose} variant='primary'>
-        Nice, that&#39;s all :&#41;
+      {/* Image Element */}
+      <img src={imageSrc} alt={`${program} Option`} className={styles.image} />
+      <Button className={styles.button} onClick={handleClose} variant="primary">
+        Nice, that&#39;s all :)
       </Button>
     </div>
   );
