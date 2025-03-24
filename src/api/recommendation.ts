a618df0b277f7d21b448ee34ee3590d8be1a41a5
ericzhang11101 <ericrenzhang321@gmail.com>
2025-03-24 04:57:55 -0400

    url

--- src/api/recommendation.ts
+++ src/api/recommendation.ts
@@ -1,9 +1,13 @@
 // const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
+const URL =
+  process.env.NEXT_REC_URL ||
+  "https://qpsvyvfwwg.execute-api.us-east-2.amazonaws.com/default/forward-req";
 
 export const getRecommendations = async (email: string, option: string) => {
   // const response = await fetch(`${BASE_URL}/recommendation/`, {
   const response = await fetch(
-    "https://dolj5cdchjb7ikn3nj74tn5ica0ipjlq.lambda-url.us-east-2.on.aws/",
+    // "https://dolj5cdchjb7ikn3nj74tn5ica0ipjlq.lambda-url.us-east-2.on.aws/",
+    URL,
     {
       method: "POST",
       headers: {
