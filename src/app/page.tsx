dd5119cfc65b92aacd2c646b28e4ca7a14d3c52e
owen-sellner <owen.sellner@gmail.com>
2025-03-24 06:15:11 -0400

    fix faq styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
 "use client";
+
 import Accordion from "@/components/Accordion";
 import FileUpload from "@/components/FileUpload";
+import Footer from "@/components/Footer";
 import HorizontalScroll from "@/components/HorizontalScroll";
 import NavBar from "@/components/NavBar";
 import Quiz from "@/components/Quiz";
 import { useAuth } from "@/context/AuthContext";
 import { useFile } from "@/context/FileContext";
 import { Box, Text } from "@primer/react";
+
+const faqData = [
+  {
+    title: "How do I complete an option?",
+    content:
+      "You complete an option once you have fulfilled all the course requirements for an option and declared the option with your academic advisor.",
+  },
+  {
+    title: "How do I declare an option?",
+    content:
+      "Declare the option by completing all 3 sections of the Plan Modification Form, and email it to your academic advisor with your name, student number, and the name of the option.",
+  },
+  {
+    title: "When should I declare an option?",
+    content:
+      "There is no set limit on when you can declare an option. If you happen to be taking courses that meet an option's requirements, you can declare that option as late as 4B. However, it is recommended that you begin taking courses that fulfill a desired option's requirements as early as 2A or 2B.",
+  },
+  {
+    title: "What requirements do I need to complete an option?",
+    content:
+      "Each option has a predefined list of required and elective courses. You can find this information on the University of Waterloo's official website, or you can use Opt'On to explore eligible courses based on your current transcript.",
+  },
+  {
+    title: "Can I complete more than one option?",
+    content:
+      "Yes, but you may need to overload your terms or choose options with overlapping course requirements.",
+  },
+  {
+    title: "Can I have a course outside the options list count towards completion?",
+    content:
+      "Yes, you can! To do so, your proposed overridden course(s) must share at least 70% similarity with MSE material. Relevant research with the University may also count towards an option. Afterwards, you must contact your options coordinator and include your proposed courses, the matching MSE courses, and an outline/description for all courses.",
+  },
+];
 
 export default function Home() {
   const { userInfo } = useAuth();
@@ -23,10 +58,22 @@ export default function Home() {
           overflow='hidden'
           paddingTop='32px'
         >
-          <Text as='h1' style={{ fontWeight: 500, fontSize: 40, lineHeight: "140%" }}>
+          <Text
+            as='h1'
+            style={{
+              fontWeight: 500,
+              fontSize: 40,
+              lineHeight: "140%",
+              textAlign: "center",
+              marginBottom: "32px",
+            }}
+          >
             Get more out of your degree
           </Text>
-          <Text as='h2' style={{ color: "#57606A", fontWeight: 400, fontSize: 20 }}>
+          <Text
+            as='h2'
+            style={{ color: "#57606A", fontWeight: 400, fontSize: 20, textAlign: "center" }}
+          >
             We got you - we&#39;re making options planning easier ✌️
           </Text>
           {!userInfo?.program && (
@@ -69,52 +116,29 @@ export default function Home() {
           >
             <HorizontalScroll />
           </Box>
-          <Box style={{ height: "100vh", width: "100%", margin: "auto" }}>
+          <Box style={{ paddingBottom: "224px", width: "100%", margin: "auto" }}>
             <Text as='h2' style={{ fontSize: "32px", textAlign: "center", marginBottom: "32px" }}>
               Frequently asked questions
             </Text>
-            <Accordion
-              title='How do I complete an option?'
-              content='You complete an option once you have fulfilled all the course requirements for an option and declared the option with your academic advisor.'
-            />
-            <hr
-              style={{ borderColor: "#d8dee4", borderWidth: "2px", width: "40%", margin: "auto" }}
-            />
-            <Accordion
-              title='How do I declare an option?'
-              content='Declare the option by completing all 3 sections of the Plan Modification Form, and email it to your academic advisor with your name, student number, and the name of the option.'
-            />
-            <hr
-              style={{ borderColor: "#d8dee4", borderWidth: "2px", width: "40%", margin: "auto" }}
-            />
-            <Accordion
-              title='When should I declare an option?'
-              content="There is no set limit on when you can declare an option. If you happen to be taking courses that meet an option's requirements, you can declare that option as late as 4B. However, it is recommended that you begin taking courses that fulfill a desired option\'s requirements as early as 2A or 2B."
-            />
-            <hr
-              style={{ borderColor: "#d8dee4", borderWidth: "2px", width: "40%", margin: "auto" }}
-            />
-            <Accordion
-              title='What requirements do I need to complete an option?'
-              content="Each option has a predefined list of required and elective courses. You can find this information on the University of Waterloo's official website, or you can use Opt'On to explore eligible courses based on your current transcript."
-            />
-            <hr
-              style={{ borderColor: "#d8dee4", borderWidth: "2px", width: "40%", margin: "auto" }}
-            />
-            <Accordion
-              title='Can I complete more than one option?'
-              content='Yes, but you may need to overload your terms or choose options with overlapping course requirements.'
-            />
-            <hr
-              style={{ borderColor: "#d8dee4", borderWidth: "2px", width: "40%", margin: "auto" }}
-            />
-            <Accordion
-              title='Can I have a course outside the options list count towards completion?'
-              content='Yes, you can! To do so, your proposed overridden course(s) must share at least 70% similarity with MSE material. Relevant research with the University may also count towards an option. Afterwards, you must contact your options coordinator and include your proposed courses, the matching MSE courses, and an outline/description for all courses.'
-            />
+            {faqData.map((faq, index) => (
+              <div key={index}>
+                <Accordion title={faq.title} content={faq.content} />
+                {index < faqData.length - 1 && (
+                  <hr
+                    style={{
+                      borderColor: "#d8dee4",
+                      borderWidth: "1px",
+                      maxWidth: "600px",
+                      margin: "auto",
+                    }}
+                  />
+                )}
+              </div>
+            ))}
           </Box>
         </Box>
       </section>
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@primer/react";
+
+export default function Footer() {
+  return <Box style={{ width: "100%", background: "red" }}>test</Box>;
+}
